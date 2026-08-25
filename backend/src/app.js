9298571dd98d61e5385fb7a76516c665801b22f7
mehdi-bellache <wassimbelll10@gmail.com>
2026-08-25 14:16:28 +0200

    Establish a connection with the database in app file

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,7 @@
+import "dotenv/config";
 import express from "express";
 import productsRoutes from "./routes/productsRoutes.js";
+import connectDB from "./db/connect.js";
 
 const app = express();
 
@@ -9,6 +11,15 @@ app.use("/api/v1/products", productsRoutes);
 
 const port = 3000;
 
-app.listen(port, () => {
-  console.log(`App is listening on port ${port}...`);
-});
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`App is listening on port ${port}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
