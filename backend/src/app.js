653a3d69cbe906413f565dcbf86b6518b1da5e42
mehdi-bellache <wassimbelll10@gmail.com>
2026-08-25 13:13:35 +0200

    Add a gitignore, install express and mongoose, update appjs

---
+++ backend/src/app.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+const port = 3000;
+
+app.get("")
+
+app.listen(port, () => {
+  console.log(`App is listening on port ${port}...`);
+});
