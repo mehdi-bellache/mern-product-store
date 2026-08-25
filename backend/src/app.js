e60ba0c7896e090eacb16bdee1786428c618237a
mehdi-bellache <wassimbelll10@gmail.com>
2026-08-25 17:51:35 +0200

    install the package cors and use it in app file

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,13 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
+
 import productsRoutes from "./routes/productsRoutes.js";
 import connectDB from "./db/connect.js";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1/products", productsRoutes);
