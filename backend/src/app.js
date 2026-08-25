6638263d09233ea1a7685afb698f6c174916f1d1
mehdi-bellache <wassimbelll10@gmail.com>
2026-08-25 13:25:00 +0200

    Add routes in app file

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,9 +2,29 @@ import express from "express";
 
 const app = express();
 
-const port = 3000;
+app.use(express.json());
 
-app.get("")
+app.get("/api/v1/products", (req, res) => {
+  res.send("All products");
+});
+
+app.get("/api/v1/products/:id", (req, res) => {
+  res.send("One product");
+});
+
+app.post("/api/v1/products", (req, res) => {
+  res.send("product created");
+});
+
+app.patch("/api/v1/products", (req, res) => {
+  res.send("product updated");
+});
+
+app.delete("/api/v1/products/:id", (req, res) => {
+  res.send("product deleted");
+});
+
+const port = 3000;
 
 app.listen(port, () => {
   console.log(`App is listening on port ${port}...`);
