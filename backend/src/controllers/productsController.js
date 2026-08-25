b4f2c1b92ef11f46b61fc23ed028163a32a41451
mehdi-bellache <wassimbelll10@gmail.com>
2026-08-25 15:01:35 +0200

    Install the package http-status-codes and import it in productsController

--- backend/src/controllers/productsController.js
+++ backend/src/controllers/productsController.js
@@ -1,7 +1,14 @@
 import express from "express";
+import Product from "../models/Product.js";
+import { StatusCodes } from "http-status-codes";
 
-export const getAllProducts = (req, res) => {
-  res.send("All products");
+export const getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(StatusCodes.OK).json({ products });
+  } catch (error) {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).send("Internal Server Error");
+  }
 };
 
 export const getProduct = (req, res) => {
--- backend/src/models/Product.js
+++ backend/src/models/Product.js
@@ -10,11 +10,9 @@ const productSchema = new mongoose.Schema(
       maxLength: [25, "name can not be more than 25 characters"],
     },
     price: {
-      type: String,
+      type: Number,
       required: [true, "must provide a price"],
       trim: true,
-      //   minLength: [3, "name can not be less than 3 characters"],
-      //   maxLength: [25, "name can not be more than 25 characters"],
     },
     image: {
       type: String,
@@ -25,6 +23,6 @@ const productSchema = new mongoose.Schema(
   { timestamps: true }, // createdAt updatedAt
 );
 
-const Note = mongoose.model("Note", productSchema);
+const Product = mongoose.model("Product", productSchema);
 
-export default Note;
+export default Product;
