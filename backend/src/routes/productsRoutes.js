92c7716796f7c13e1990df61c46e8e0aac7a23f1
mehdi-bellache <wassimbelll10@gmail.com>
2026-08-25 13:38:48 +0200

    Add the file productsRoutes

---
+++ backend/src/routes/productsRoutes.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+import {
+  getAllProducts,
+  getProduct,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+} from "../controllers/productsController";
+
+const router = express.Router();
+
+router.get("/", getAllProducts);
+router.get("/:id", getProduct);
+router.post("/", createProduct);
+router.patch("/:id", updateProduct);
+router.delete("/:id", deleteProduct);
+
+export default router;
