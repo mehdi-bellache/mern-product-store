1d6cc9317cc47736229dc8d67a13a3407ca51aa7
mehdi-bellache <wassimbelll10@gmail.com>
2026-08-26 22:13:31 +0200

    import ProductNotDFound in the home page

--- frontend/src/components/ProductsNotFound.jsx
+++ frontend/src/components/ProductsNotFound.jsx
@@ -1,21 +1,9 @@
-import Navbar from "./Navbar"
-import { Rocket } from "lucide-react"
-import { useEffect } from "react";
 import { Link } from "react-router-dom";
-import axios from "axios";
 
 const ProductsNotFound = () => {
-    useEffect(() =>{
-        const fetchProducts = async ()=>{
-            await axios.get("")
 
-
-        }
-    })
   return (
     <div>
-        <Navbar/>
-        <h1>Current Products <Rocket/></h1>
         <span>No products found </span>
         <Link to={"/create"} className="btn btn-primary">
             <span>Create A Product</span>
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
 import Navbar from '../components/Navbar'
+import axios from "axios";
+import { useEffect, useState } from "react";
+import toast from 'react-hot-toast';
+import ProductsNotFound from '../components/ProductsNotFound';
+
+
 
 const HomePage = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() =>{
+    const fetchProducts = async ()=>{
+      try{
+        products = await axios.get("http://localhost:3000/api/v1/products");
+      }
+      catch(error){
+        toast.error("Error while fetching the products");
+      }
+    }; fetchProducts()
+  }, []);
+
   return (
     <div>
         <Navbar/>
+        <h1>Current Products <Rocket/></h1>
+        {products.length === 0 ?  <ProductsNotFound/> : "products exists"}
       
     </div>
   )
