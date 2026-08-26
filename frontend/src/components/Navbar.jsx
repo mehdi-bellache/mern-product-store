24f03512799033d7365804a5dcfe67a83ed21c1a
mehdi-bellache <wassimbelll10@gmail.com>
2026-08-26 12:41:03 +0200

    Add useEffect and useState and add the function toggleColorMode in Navbar file

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,19 +1,18 @@
-import {ShoppingCart, PlusSquare, Sun, FileX} from "lucide-react";
-import { Link } from "react-router-dom"; 
-
-
-/** 
- * pour header :
- * display flex
- * align items center 
- * justify-content space-between
- * pour : 
- * plus spuare : margin-right 5px
- * plus-square et l'autre : backaground color: 
- * 
-*/
+import {ShoppingCart, PlusSquare, Sun, Moon} from "lucide-react";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [theme, setTheme] = useState("coffee");
+
+  const toggleColorMode = ()=>{
+    setTheme(theme === "coffee" ? "light" : "coffee") ;
+  }
+
+  useEffect(()=>{
+    document.documentElement.setAttribute('data-theme', theme);
+  }, [theme]);
+
   return (
     <header className='bg-base-300 border-b border-base-content/10'>
         <div className="mx-auto max-w-6xl p-4">
@@ -21,14 +20,15 @@ const Navbar = () => {
               <h1 className="text-3xl font-bold text-primary flex items-center gap-2">
                 Product Store <ShoppingCart className="size-8"/>
               </h1>
-                <div className="flex items-center gap-4">
-                    <Link to={"/create"} className="btn btn-primary">
-                       <PlusSquare className="size-5"/>
-                    </Link>
-                    <Link to={"/"} className="btn btn-primary">
-                       <Sun className="size-5"/>
-                    </Link>
-                </div>
+      
+              <div className="flex items-center gap-4">
+                <Link to={"/create"} className="btn btn-primary">
+                  <PlusSquare className="size-5"/>
+                </Link>
+                <button onClick={toggleColorMode} className="btn btn-primary">
+                  { theme === "coffee" ? <Sun className="size-5"/> : <Moon className="size-5"/>}
+                </button>
+              </div>
             </div>
         </div>
     </header>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,6 +6,6 @@ export default {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["coffee", "dark", "aqua", "cyberpunk"],
+    themes: ["coffee", "light", "dark", "aqua", "cyberpunk"],
   },
 };
