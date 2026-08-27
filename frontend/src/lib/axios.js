1cbb9888d309df5f9e19051c959be5b17b003df1
mehdi-bellache <wassimbelll10@gmail.com>
2026-08-27 19:06:06 +0200

    Add the file axios.js

---
+++ frontend/src/lib/axios.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:3000/api/v1"
+    : "/api/v1";
+const api = axios.create({
+  baseURL: BASE_URL,
+});
+
+export default api;
