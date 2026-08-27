a7beefa8da9808278188b67442600c74cf1edf42
mehdi-bellache <wassimbelll10@gmail.com>
2026-08-27 16:53:16 +0200

    import path and use it in app file

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,20 +1,29 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import path from "path";
 
 import productsRoutes from "./routes/productsRoutes.js";
 import connectDB from "./db/connect.js";
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  }),
-);
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    }),
+  );
+}
 app.use(express.json());
 
 app.use("/api/v1/products", productsRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  const frontendDistPath = path.join(__dirname, "../frontend/dist");
+  app.use(express.static(frontendDistPath, "index.html"));
+}
 
 const port = process.env.PORT || 3000;
 
